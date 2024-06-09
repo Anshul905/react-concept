ef0d2801323f1131f1b65aa66e0eb57690fc2439
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-06-09 13:50:42 +0530

    NavLink Component & Default Active Class

--- router/src/pages/Home.jsx
+++ router/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Home() {
   return (
@@ -10,13 +10,13 @@ function Home() {
       <nav>
         <ul >
           <li>
-            <Link to="/"> Home </Link> 
+            <NavLink to="/" > Home </NavLink> 
           </li>
           <li>
-            <Link to="/about"> About </Link> 
+            <NavLink to="/about"> About </NavLink> 
           </li>
           <li>
-            <Link to="/contact"> Contact </Link> 
+            <NavLink to="/contact"> Contact </NavLink> 
           </li>
         </ul>
       </nav>
