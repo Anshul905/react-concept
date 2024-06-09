947f764f63971b4fef27a9ea6d1aabc5dc6ff409
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-06-09 16:39:09 +0530

    useNavigate Hook functionality

--- router/src/pages/Home.jsx
+++ router/src/pages/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  const navigate = useNavigate()
+  const goToContactPage = () => {
+    navigate("/contact")
+  }
+  const goToContactAboutUsPage = () => {
+    navigate("/about")
+  }
+  
+  const goToPreviouspage = () => {
+    navigate(-1)
+  }
+
   return (
     <>    
     <section className='text-center font-bold text-4xl mt-10'>
       <h1>Home Page</h1>
     </section>
+    <div className="flex justify-center mt-5 gap-5">
+      <button onClick={ goToContactAboutUsPage }>Go to About Us Page</button>
+      <button onClick={ () => navigate("/contact") }>Go to contact Page</button>
+      <button onClick={ () => goToPreviouspage() }>Go to Previous Page</button>
+    </div>
     </>
 )
 }
